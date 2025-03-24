7ff864c8ce4d968a309d3e01d8e06ef8d3ca8661
Bhavyam <bhavyamsanghavi@gmail.com>
2025-03-24 22:35:07 +0530

    Port binding

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes.auth import router as auth_router
 from app.routes.ads import router as ads_router
+import uvicorn
 # from app.routes.auth import router as ads_router
 
 app = FastAPI()
@@ -12,3 +13,6 @@ app.include_router(ads_router, prefix="/ads", tags=["Google Ads"])
 @app.get("/")
 async def root():
     return {"message": "Google Ads Integration API"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
